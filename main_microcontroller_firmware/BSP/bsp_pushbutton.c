c74a92b9b4dfd37a161f303c302a12084f379461
Jordan Aceto <ja532@cornell.edu>
2024-10-02 16:43:51 -0400

    Add code to handle all button states, buttons are not debounced yet

--- main_microcontroller_firmware/BSP/bsp_pushbutton.c
+++ main_microcontroller_firmware/BSP/bsp_pushbutton.c
@@ -17,16 +17,66 @@ void pushbuttons_init()
 
 Button_State_t user_pushbutton_state()
 {
-    // TODO, code to debounce the switch and get the other states
+    static bool last_user_button_state = false;
+
+    // TODO, code to debounce the switch
 
     // note: the button has a pullup and pressing the switch shorts it to ground, so the action is inverting
-    return gpio_read_pin(&bsp_pins_user_pushbutton_cfg) ? BUTTON_STATE_NOT_PRESSED : BUTTON_STATE_PRESSED;
+    const bool this_button_state = !gpio_read_pin(&bsp_pins_user_pushbutton_cfg);
+
+    Button_State_t retval;
+
+    if (!last_user_button_state && this_button_state)
+    {
+        retval = BUTTON_STATE_JUST_PRESSED;
+    }
+    else if (last_user_button_state && this_button_state)
+    {
+        retval = BUTTON_STATE_PRESSED;
+    }
+    else if (last_user_button_state && !this_button_state)
+    {
+        retval = BUTTON_STATE_JUST_RELEASED;
+    }
+    else
+    {
+        retval = BUTTON_STATE_NOT_PRESSED;
+    }
+
+    last_user_button_state = this_button_state;
+
+    return retval;
 }
 
 Button_State_t ble_enable_pushbutton_state()
 {
-    // TODO, code to debounce the switch and get the other states
+    static bool last_ble_button_state = false;
+
+    // TODO, code to debounce the switch
 
     // note: the button has a pullup and pressing the switch shorts it to ground, so the action is inverting
-    return gpio_read_pin(&bsp_pins_ble_en_pushbutton_cfg) ? BUTTON_STATE_NOT_PRESSED : BUTTON_STATE_PRESSED;
+    const bool this_button_state = !gpio_read_pin(&bsp_pins_ble_en_pushbutton_cfg);
+
+    Button_State_t retval;
+
+    if (!last_ble_button_state && this_button_state)
+    {
+        retval = BUTTON_STATE_JUST_PRESSED;
+    }
+    else if (last_ble_button_state && this_button_state)
+    {
+        retval = BUTTON_STATE_PRESSED;
+    }
+    else if (last_ble_button_state && !this_button_state)
+    {
+        retval = BUTTON_STATE_JUST_RELEASED;
+    }
+    else
+    {
+        retval = BUTTON_STATE_NOT_PRESSED;
+    }
+
+    last_ble_button_state = this_button_state;
+
+    return retval;
 }
