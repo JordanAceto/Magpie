3e337cb7fef1692c07ffd43228c9e15f3dc2d397
Jordan Aceto <ja532@cornell.edu>
2024-10-31 10:24:08 -0400

    Only do basic pin config and clk enable in BSP SPI, delegate SPI init to ADC module

--- main_microcontroller_firmware/BSP/bsp_spi.c
+++ main_microcontroller_firmware/BSP/bsp_spi.c
@@ -8,47 +8,28 @@
 #include "bsp_spi.h"
 #include "mxc_errors.h"
 
+/* Private function declarations -------------------------------------------------------------------------------------*/
+
+/**
+ * `reset_spi_and_en_periph_clk(hspi)` enables the peripheral clock and resets SPI peripheral `hspi`.
+ *
+ * This function is necessary due to the definition of `MSDK_NO_GPIO_CLK_INIT`, it replicates the code in the MSDK SPI
+ * init functions which is not compiled due to the `MSDK_NO_GPIO_CLK_INIT` definition.
+ *
+ * @retval `E_NO_ERROR` if successful, else a negative error code
+ */
+static int reset_spi_and_en_periph_clk(const mxc_spi_regs_t *hspi);
+
 /* Public function definitions ---------------------------------------------------------------------------------------*/
 
 int bsp_adc_config_spi_init()
 {
-    MXC_GPIO_Config(&bsp_pins_adc_cfg_spi_active_cfg);
+    MXC_GPIO_Config(&bsp_pins_adc_cfg_spi_active_cfg); // spi pins
 
-    MXC_GPIO_Config(&bsp_pins_adc_cfg_spi_cs_out_cfg);
-    gpio_write_pin(&bsp_pins_adc_cfg_spi_cs_out_cfg, true);
+    MXC_GPIO_Config(&bsp_pins_adc_cfg_spi_cs_out_cfg);      // manual chip select pin
+    gpio_write_pin(&bsp_pins_adc_cfg_spi_cs_out_cfg, true); // chip select high at startup
 
-    MXC_SYS_ClockEnable(bsp_pins_adc_cfg_spi_clock_enum);
-
-    int res = E_NO_ERROR;
-
-    // freq doesn't matter too much, we only init rarely and write to a few registers
-    const uint32_t CFG_SPI_CLK_FREQ_Hz = 5000000;
-
-    if ((res = MXC_SPI_Init(
-             bsp_pins_adc_cfg_spi_handle,
-             1, // 1 -> master mode
-             0, // 0 -> quad mode not used, single bit SPI
-             1, // num slaves
-             0, // CS polarity (0 for active low)
-             CFG_SPI_CLK_FREQ_Hz,
-             MAP_A)) != E_NO_ERROR)
-    {
-        return res;
-    }
-    if ((res = MXC_SPI_SetDataSize(bsp_pins_adc_cfg_spi_handle, 8)) != E_NO_ERROR)
-    {
-        return res;
-    }
-    if ((res = MXC_SPI_SetWidth(bsp_pins_adc_cfg_spi_handle, SPI_WIDTH_STANDARD)) != E_NO_ERROR)
-    {
-        return res;
-    }
-    if ((res = MXC_SPI_SetMode(bsp_pins_adc_cfg_spi_handle, SPI_MODE_0)) != E_NO_ERROR)
-    {
-        return res;
-    }
-
-    return E_NO_ERROR;
+    return reset_spi_and_en_periph_clk(bsp_pins_adc_cfg_spi_handle);
 }
 
 int bsp_adc_config_spi_deinit()
@@ -57,8 +38,6 @@ int bsp_adc_config_spi_deinit()
 
     MXC_GPIO_Config(&bsp_pins_adc_cfg_spi_cs_high_z_cfg);
 
-    MXC_SYS_ClockDisable(bsp_pins_adc_cfg_spi_clock_enum);
-
     return MXC_SPI_Shutdown(bsp_pins_adc_cfg_spi_handle);
 }
 
@@ -66,39 +45,12 @@ int bsp_adc_ch0_data_spi_init()
 {
     MXC_GPIO_Config(&bsp_pins_adc_ch0_data_spi_active_cfg);
 
-    MXC_SYS_ClockEnable(bsp_pins_adc_ch0_data_spi_clock_enum);
-
-    int res = E_NO_ERROR;
-
-    if ((res = MXC_SPI_Init(
-             bsp_pins_adc_ch0_data_spi_handle,
-             0, // 0 -> slave mode
-             0, // 0 -> quad mode not used, single bit SPI
-             0, // num slaves, none
-             0, // CS polarity (0 for active low)
-             0, // freq is defined by the driving clock
-             MAP_A)) != E_NO_ERROR)
-    {
-        return res;
-    }
-
-    if ((res = MXC_SPI_SetWidth(bsp_pins_adc_ch0_data_spi_handle, SPI_WIDTH_3WIRE)) != E_NO_ERROR)
-    {
-        return res;
-    }
-    if ((res = MXC_SPI_SetMode(bsp_pins_adc_ch0_data_spi_handle, SPI_MODE_1)) != E_NO_ERROR)
-    {
-        return res;
-    }
-
-    return E_NO_ERROR;
+    return reset_spi_and_en_periph_clk(bsp_pins_adc_ch0_data_spi_handle);
 }
 
 int bsp_adc_ch0_data_spi_deinit()
 {
     MXC_GPIO_Config(&bsp_pins_adc_ch0_data_spi_high_z_cfg);
-
-    MXC_SYS_ClockDisable(bsp_pins_adc_ch0_data_spi_clock_enum);
 
     return MXC_SPI_Shutdown(bsp_pins_adc_ch0_data_spi_handle);
 }
@@ -111,6 +63,43 @@ int bsp_adc_ch1_data_spi_init()
 
 int bsp_adc_ch1_data_spi_deinit()
 {
-    // TODO
-    return -1;
+    MXC_GPIO_Config(&bsp_pins_adc_ch1_data_spi_high_z_cfg);
+
+    return MXC_SPI_Shutdown(bsp_pins_adc_ch1_data_spi_handle);
+}
+
+/* Private function definitions --------------------------------------------------------------------------------------*/
+
+int reset_spi_and_en_periph_clk(const mxc_spi_regs_t *hspi)
+{
+    if (hspi == MXC_SPI0)
+    {
+        MXC_GCR->rstr1 |= MXC_F_GCR_RSTR1_SPI0;
+        while (MXC_GCR->rstr1 & MXC_F_GCR_RSTR1_SPI0)
+        {
+        }
+        MXC_GCR->perckcn1 &= ~(MXC_F_GCR_PERCKCN1_SPI0D);
+    }
+    else if (hspi == MXC_SPI1)
+    {
+        MXC_GCR->rstr0 |= MXC_F_GCR_RSTR0_SPI1;
+        while (MXC_GCR->rstr0 & MXC_F_GCR_RSTR0_SPI1)
+        {
+        }
+        MXC_GCR->perckcn0 &= ~(MXC_F_GCR_PERCKCN0_SPI1D);
+    }
+    else if (hspi == MXC_SPI2)
+    {
+        MXC_GCR->rstr0 |= MXC_F_GCR_RSTR0_SPI2;
+        while (MXC_GCR->rstr0 & MXC_F_GCR_RSTR0_SPI2)
+        {
+        }
+        MXC_GCR->perckcn0 &= ~(MXC_F_GCR_PERCKCN0_SPI2D);
+    }
+    else
+    {
+        return E_BAD_PARAM;
+    }
+
+    return E_NO_ERROR;
 }
--- main_microcontroller_firmware/BSP/bsp_spi.h
+++ main_microcontroller_firmware/BSP/bsp_spi.h
@@ -13,11 +13,12 @@
 /* Public function declarations --------------------------------------------------------------------------------------*/
 
 /**
- * @brief `bsp_adc_config_spi_init()` initializes and starts SPI2 bus as a master.
+ * @brief `bsp_adc_config_spi_init()` enables and initializes the GPIO pins for the SPI bus responsible for configuring
+ * the ADC.
  *
  * @pre the LDOs are powered on via bsp_power_on_LDOs().
  *
- * @post QSPI2 is initialized and ready to send config data to the ADC.
+ * @post The ADC config SPI is initialized and ready to send config data to the ADC.
  *
  * @retval Success/Fail, see MXC_Error_Codes for a list of return codes.
  */
@@ -26,18 +27,19 @@ int bsp_adc_config_spi_init();
 /**
  * @brief `bsp_adc_config_spi_deinit()` de-initializes the config SPI and sets the associated pins to high-Z.
  *
- * @post QSPI2 is de-initialized and the GPIO pins associated with the bus are high-Z.
+ * @post The ADC config SPI is de-initialized and the GPIO pins associated with the bus are high-Z.
  *
  * @retval Success/Fail, see MXC_Error_Codes for a list of return codes.
  */
 int bsp_adc_config_spi_deinit();
 
 /**
- * @brief `bsp_adc_ch0_data_spi_init()` initializes and starts SPI1 bus as a slave which received data from the ADC.
+ * @brief `bsp_adc_ch0_data_spi_init()` enables and initializes the SPI bus responsible for reading audio data from the
+ * ADC channel 0.
  *
  * @pre the LDOs are powered on via bsp_power_on_LDOs().
  *
- * @post QSPI1 is initialized and ready to read data from the ADC.
+ * @post The ADC channel-0 date SPI bus is initialized and ready to read data from the ADC.
  *
  * @retval Success/Fail, see MXC_Error_Codes for a list of return codes.
  */
@@ -46,18 +48,19 @@ int bsp_adc_ch0_data_spi_init();
 /**
  * @brief `bsp_adc_ch0_data_spi_deinit()` de-initializes the ch0 data SPI and sets the associated pins to high-Z.
  *
- * @post QSPI1 is de-initialized and the GPIO pins associated with the bus are high-Z.
+ * @post The ADC channel-0 date SPI bus is de-initialized and the GPIO pins associated with the bus are high-Z.
  *
  * @retval Success/Fail, see MXC_Error_Codes for a list of return codes.
  */
 int bsp_adc_ch0_data_spi_deinit();
 
 /**
- * @brief `bsp_adc_ch1_data_spi_init()` initializes and starts SPI0 bus as a slave which received data from the ADC.
+ * @brief `bsp_adc_ch1_data_spi_init()` enables and initializes the SPI bus responsible for reading audio data from the
+ * ADC channel 1.
  *
  * @pre the LDOs are powered on via bsp_power_on_LDOs().
  *
- * @post QSPI0 is initialized and ready to read data from the ADC.
+ * @post The ADC channel 1 data SPI bus is initialized and ready to read data from the ADC.
  *
  * @retval Success/Fail, see MXC_Error_Codes for a list of return codes.
  */
@@ -66,7 +69,7 @@ int bsp_adc_ch1_data_spi_init();
 /**
  * @brief `bsp_adc_ch1_data_spi_deinit()` de-initializes the ch1 data SPI and sets the associated pins to high-Z.
  *
- * @post QSPI0 is de-initialized and the GPIO pins associated with the bus are high-Z.
+ * @post The ADC channel 1 data SPI bus is de-initialized and the GPIO pins associated with the bus are high-Z.
  *
  * @retval Success/Fail, see MXC_Error_Codes for a list of return codes.
  */
